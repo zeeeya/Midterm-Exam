e2b6de4a30b1828269e3c4cc1f316734a22be4c9
zeeeya <zci.alvarez@gmail.com>
2018-02-05 21:55:19 +0800

    Modified environements module

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,11 @@
-
+export const environment = {
+    production: true,
+    firebaseConfig: {
+     apiKey: "AIzaSyCNsDcXrhXwF4iM5t3RB4fsLh12Yfsq4sQ",
+    authDomain: "midterm-exam-3bdcb.firebaseapp.com",
+    databaseURL: "https://midterm-exam-3bdcb.firebaseio.com",
+    projectId: "midterm-exam-3bdcb",
+    storageBucket: "midterm-exam-3bdcb.appspot.com",
+    messagingSenderId: "834401394828"
+    }
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+    apiKey: "AIzaSyCNsDcXrhXwF4iM5t3RB4fsLh12Yfsq4sQ",
+    authDomain: "midterm-exam-3bdcb.firebaseapp.com",
+    databaseURL: "https://midterm-exam-3bdcb.firebaseio.com",
+    projectId: "midterm-exam-3bdcb",
+    storageBucket: "midterm-exam-3bdcb.appspot.com",
+    messagingSenderId: "834401394828"
+
     }
 };
